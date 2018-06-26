f37eb754f914e6717e1eb4c000b361f138442f70
Kimora Kong <kekong99@fb.com>
2018-06-26 13:16:23 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Kimora Kong on 6/26/18.
+//  Copyright © 2018 Kimora Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
